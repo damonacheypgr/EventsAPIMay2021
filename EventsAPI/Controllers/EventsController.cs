a9ed8876372f17f90ea268244c376f6606e99a56
Jeffry Gonzalez <jeff@hypertheory.com>
2021-05-03 16:57:01 -0400

    done with monday

--- EventsAPI/Controllers/EventsController.cs
+++ EventsAPI/Controllers/EventsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,6 +19,64 @@ namespace EventsAPI.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}/participants")] // GET /events/1/partipants
+        public async Task<ActionResult> GetPartipantsForEvent(int id)
+        {
+            var savedEvent = await _context.Events.Where(e => e.Id == id).SingleOrDefaultAsync();
+            if (savedEvent == null)
+            {
+                return NotFound();
+            }
+            // ??? Run the query again?
+            var participants = await _context.Events
+                .Where(e => e.Id == id)
+                .Select(e => e.Participants)
+                .ToListAsync();
+
+            return Ok(participants); // Bad again!
+
+
+
+        }
+
+
+        [HttpPost]
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration = 10)]
+        public async Task<ActionResult> AddEvent([FromBody] PostEventRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                // Add it to the database
+                // Return a 201 with a Location  Header (hard to do right now... but we will)
+                var eventToAdd = new Event()
+                {
+                    Name = request.Name,
+                    HostedBy = request.HostedBy,
+                    LongDescription = request.LongDescription,
+                    StartDateAndTime = request.StartDateAndTime.Value,
+                    EndDateAndTime = request.EndDateAndTime.Value
+                };
+                _context.Events.Add(eventToAdd);
+                await _context.SaveChangesAsync();
+                //return Created()
+                //var url = Url.Action(nameof(EventsController.GetById), nameof(EventsController), new { id = eventToAdd.Id });
+                //return Created(url, eventToAdd); 
+                return CreatedAtRoute("get-event-by-id", new { id = eventToAdd.Id }, eventToAdd); // this is a bit wrong.. stay with me.
+            }
+
+        }
+
+        [HttpGet("{id:int}", Name = "get-event-by-id")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            return Ok();
+        }
+
 
         [HttpGet] // GET /events, GET /events?showPast=true
         public async Task<ActionResult> Get([FromQuery] bool showPast = false)
@@ -35,4 +94,16 @@ namespace EventsAPI.Controllers
     public record GetResponse<T>(IList<T> Data);
 
     public record GetEventsResponseItem(int Id, string Name, DateTime StartDate, DateTime EndDate, int NumberOfParticipants);
+
+    public record PostEventRequest(
+        [Required]
+        string Name,
+        string LongDescription,
+        [Required]
+        string HostedBy,
+        [Required]
+        DateTime? StartDateAndTime,
+        [Required]
+        DateTime? EndDateAndTime
+        );
 }
